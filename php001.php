420f502487c268e0196116fff85bdc3a00b3fe35
J.Andrey <106285487+Andreysamura@users.noreply.github.com>
2022-07-07 16:14:57 -0500

    Unidad III. Tarea 1

---
+++ php001.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo "<h1>Jose Andrey Dominguez Cruz</h1>";
+            echo "<h2> Tercer Cuatrimestre</h2>";
+        ?> 
+    </body>
+</html>
